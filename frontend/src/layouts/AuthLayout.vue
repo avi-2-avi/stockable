035fdc124dafe85a5bcce059c0d650ce088ef69a
Cristina <avicon95@protonmail.com>
2025-03-06 02:23:36 -0500

    feat: authentication page design for login and register

---
+++ frontend/src/layouts/AuthLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col h-screen">
+        <BaseNavbar />
+        <main class="flex-1 container mx-auto">
+            <slot />
+        </main>
+        <LandingFooter />
+    </div>
+</template>
+
+<script setup lang="ts">
+import BaseNavbar from '@/components/BaseNavbar.vue';
+import LandingFooter from '@/features/landing/components/LandingFooter.vue';
+</script>

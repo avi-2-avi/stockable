8970d0c2bdc0b7613a90109521877759ec200297
Cristina <avicon95@protonmail.com>
2025-03-06 02:23:36 -0500

    feat: authentication page design for login and register

--- frontend/src/features/auth/pages/LoginPage.vue
+++ frontend/src/features/auth/pages/LoginPage.vue
@@ -1,7 +1,63 @@
 <template>
-    <div>Login Page</div>
+    <AuthLayout>
+        <div class="flex items-center justify-center min-h-[calc(100vh-4rem)]">
+            <Card card-class="p-8 w-full max-w-md mx-auto text-center space-y-6">
+                <h2 class="font-bold">{{ isSignup ? "Create an Account" : "Welcome Back" }}</h2>
+                <p class="text-foreground/60">
+                    {{ isSignup ? "Sign up to start investing smarter." : "Log in to access your account." }}
+                </p>
+
+                <form @submit.prevent="handleSubmit" class="space-y-4">
+                    <BaseInput v-if="isSignup" label="Full Name" type="text" placeholder="John Doe"
+                        v-model="form.fullName" :errorMessage="errors.fullName" />
+
+                    <BaseInput label="Email" type="email" placeholder="you@example.com" v-model="form.email"
+                        :errorMessage="errors.email" />
+
+                    <BaseInput label="Password" type="password" placeholder="••••••••" v-model="form.password"
+                        :errorMessage="errors.password" />
+
+                    <Button :label="isSignup ? 'Sign Up' : 'Login'" size="lg" variant="solid" class="w-full" />
+
+                    <p class="text-sm">
+                        {{ isSignup ? "Already have an account?" : "Don't have an account?" }}
+                        <a @click="toggleMode" class="text-stock-400 cursor-pointer underline">
+                            {{ isSignup ? "Log in" : "Sign up" }}
+                        </a>
+                    </p>
+                </form>
+            </Card>
+        </div>
+    </AuthLayout>
 </template>
 
 <script setup lang="ts">
-</script>
+import { ref } from 'vue';
+import AuthLayout from '@/layouts/AuthLayout.vue';
+import Card from '@/components/Card.vue';
+import Button from '@/components/Button.vue';
+import BaseInput from '@/components/Input.vue';
+import { validateAuthForm } from '@/utils/validation.ts';
 
+const isSignup = ref(false);
+const form = ref({
+    fullName: "",
+    email: "",
+    password: ""
+});
+
+const errors = ref<Record<string, string>>({});
+
+const toggleMode = () => {
+    isSignup.value = !isSignup.value;
+    errors.value = {}; // Reset errors when switching
+};
+
+const handleSubmit = () => {
+    errors.value = validateAuthForm(form.value, isSignup.value);
+    if (Object.keys(errors.value).length === 0) {
+        console.log(isSignup.value ? "Signing up:" : "Logging in:", form.value);
+        // Proceed with API request
+    }
+};
+</script>

d76db4995b7a9e84c2b09911493eb79a590b338a
Cristina <avicon95@protonmail.com>
2025-03-15 23:20:55 -0500

    feat: testing with different nginx and docker-compose file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "ec2_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.stockable_ec2.public_ip
+output "ssh_command" {
+  value = "ssh -i \"${var.key_name}.pem\" ec2-user@${aws_instance.stockable_ec2.public_dns}"
+  description = "Use this command to SSH into your EC2 instance"
 }

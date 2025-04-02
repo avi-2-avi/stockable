63d79e16655eae3e5312973debce35e4901f6760
Cristina <avicon95@protonmail.com>
2025-04-02 11:51:43 -0500

    feat: dashboard about card replaces portafolio card

--- frontend/src/features/app/dashboard/pages/DashboardPage.vue
+++ frontend/src/features/app/dashboard/pages/DashboardPage.vue
@@ -2,7 +2,7 @@
     <AppLayout :pageTitle="`Welcome back, ${name}`" pageDescription="Here's what's happening today">
         <div class="grid grid-cols-6 grid-rows-1 gap-5">
             <div class="col-span-6 lg:col-span-4 row-span-1">
-                <PortafolioCard />
+                <AboutCard />
             </div>
             <div class="col-span-6 lg:col-span-2 lg:row-span-2">
                 <Card class="p-5 h-full">
@@ -36,13 +36,13 @@
 
 <script lang="ts" setup>
 import AppLayout from '@/layouts/AppLayout.vue';
-import PortafolioCard from '../components/PortafolioCard.vue';
 import Card from '@/components/ui/Card.vue';
 import RecentRatingItem from '../components/RecentRatingItem.vue';
 import { useAuthStore } from '@/store/authStore';
 import { useDashboardStore } from '@/store/dashboardStore';
 import DonutChartCard from '../components/DonutChartCard.vue';
 import NewsCard from '../components/NewsCard.vue';
+import AboutCard from '../components/AboutCard.vue';
 
 const authStore = useAuthStore();
 const { cachedDashboardRatings  } = useDashboardStore();
@@ -54,19 +54,19 @@ const news = [
         title: "Build Your First Stock Portfolio With These Easy Steps",
         description: "Learn the basics of creating a diversified stock portfolio, including risk management and smart investment strategies.",
         image: "/src/assets/news-1.webp",
-        link: "/blog/build-your-first-portfolio"
+        link: "https://finmasters.com/first-stock-portfolio/"
     },
     {
         title: "Understanding Stock Ratings: What Do They Mean?",
         description: "Not sure how to interpret stock ratings? We break down buy, hold, and sell recommendations so you can make informed decisions.",
         image: "/src/assets/news-2.webp",
-        link: "/blog/understanding-stock-ratings"
+        link: "https://www.investopedia.com/financial-edge/0512/understanding-analyst-ratings.aspx"
     },
     {
         title: "Top 5 Investment Mistakes Beginners Make",
         description: "Avoid common pitfalls when starting your investing journey. Learn how to navigate the market with confidence.",
         image: "/src/assets/news-3.webp",
-        link: "/blog/top-5-investment-mistakes"
+        link: "https://howmoneythinks.substack.com/p/top-5-investing-mistakes-beginners"
     }
 ];
 
